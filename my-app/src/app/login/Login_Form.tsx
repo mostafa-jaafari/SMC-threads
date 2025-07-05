19a976669e9729b0e04cee4a560a4fd02282597c
JaafariMostafa <mostafa.business.me@gmail.com>
2025-07-05 13:24:44 +0000

    add something fine

--- my-app/src/app/login/Login_Form.tsx
+++ my-app/src/app/login/Login_Form.tsx
@@ -1,25 +1,70 @@
+'use client';
+import { signIn } from "next-auth/react";
 
 
 
 export default function Login_Form() {
+    const Fixed_Login_Logos = [
+        {
+            image: "./PNG-LOGO-WHITE.png",
+            alt: "SMC Threads Logo",
+            className: "fixed -left-5 blur-[2px] -top-5 w-[120px] rotate-45"
+        },
+        {
+            image: "./PNG-LOGO-WHITE.png",
+            alt: "SMC Threads Logo",
+            className: "fixed -right-5 blur-[2px] -bottom-5 w-[120px] rotate-45"
+        },
+        // {
+        //     image: "./PNG-LOGO-WHITE.png",
+        //     alt: "SMC Threads Logo",
+        //     className: "fixed left-0 top-0 w-[60px]"
+        // },
+        // {
+        //     image: "./PNG-LOGO-WHITE.png",
+        //     alt: "SMC Threads Logo",
+        //     className: "fixed left-0 top-0 w-[60px]"
+        // },
+    ];
     return (
         <main className="w-1/3">
-            <button 
-                className="flex items-center gap-2 border 
-                    border-neutral-800 rounded-lg p-2 w-full
-                    hover:bg-black cursor-pointer
-                    bg-black/30">
-                <svg width="20" height="20" viewBox="0 0 48 48" style={{ verticalAlign: 'middle', marginRight: 8 }}>
-                    <g>
-                        <path fill="#4285F4" d="M44.5 20H24v8.5h11.7C34.7 32.9 30.1 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.9 1.1 8.1 3.1l6.4-6.4C34.5 5.1 29.6 3 24 3 12.9 3 4 11.9 4 23s8.9 20 20 20c11 0 19.7-8 19.7-20 0-1.3-.1-2.7-.3-4z"/>
-                        <path fill="#34A853" d="M6.3 14.7l7 5.1C14.7 16.2 19 13 24 13c3.1 0 5.9 1.1 8.1 3.1l6.4-6.4C34.5 5.1 29.6 3 24 3c-7.2 0-13.4 4.1-16.7 10.1z"/>
-                        <path fill="#FBBC05" d="M24 43c5.5 0 10.1-1.8 13.5-4.9l-6.2-5.1C29.6 34.7 27 36 24 36c-6.1 0-10.7-4.1-12.5-9.6l-7 5.4C6.6 39 14.7 43 24 43z"/>
-                        <path fill="#EA4335" d="M44.5 20H24v8.5h11.7c-1.1 3.1-4.1 5.5-7.7 5.5-3.1 0-5.9-1.1-8.1-3.1l-6.4 6.4C13.5 42.9 18.4 45 24 45c11 0 19.7-8 19.7-20 0-1.3-.1-2.7-.3-4z"/>
-                    </g>
-                </svg>
-                Login with Google
-            </button>
-            <div></div>
+            {Fixed_Login_Logos.map((logo, index) => {
+                return (
+                    <img 
+                        key={index}
+                        src={logo.image}
+                        alt={logo.alt}
+                        className={logo.className}
+                    />
+                )
+            })}
+            <section className="space-y-2">
+                <button 
+                    onClick={() => signIn('google')}
+                    className="flex items-center justify-center gap-4 border 
+                        border-neutral-800 rounded-lg px-4 py-3 w-full
+                        hover:bg-black cursor-pointer
+                        bg-black/30 text-md">
+                    <img 
+                        src="./GoogleIcon.png"
+                        alt="Google Logo"
+                        className="w-6 h-6"
+                    />
+                    Login with Google
+                </button>
+                <button 
+                    className="flex items-center justify-center gap-4 border 
+                        border-neutral-800 rounded-lg px-4 py-3 w-full
+                        hover:bg-black cursor-pointer
+                        bg-black/30 text-md">
+                    <img 
+                        src="./FacebookIcon.png"
+                        alt="Facebook Logo"
+                        className="w-6 h-6"
+                    />
+                    Login with Facebook
+                </button>
+            </section>
         </main>
     )
 }
--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -1,3 +1,4 @@
+import BlurText from "@/Components/TextAnimations/BlurText";
 import Login_Form from "./Login_Form";
 
 
@@ -5,10 +6,25 @@ import Login_Form from "./Login_Form";
 
 export default function page(){
     return (
-        <main className="w-full min-h-screen flex flex-col items-center py-20">
-            <h1 className="text-3xl font-bold underline">
-                Login Page
-            </h1>
+        <main className="space-y-6 w-full min-h-screen flex flex-col items-center py-20">
+            <img 
+                src="./PNG-LOGO-WHITE.png"
+                alt="SMC Threads Logo"
+                className="w-[120px] shadow-inner-overlay shadow-yellow-600"
+            />
+            <div className="text-center">
+                <BlurText
+                text="Welcome back to Saylo"
+                delay={150}
+                animateBy="words"
+                direction="top"
+                // onAnimationComplete={handleAnimationComplete}
+                className="text-2xl mb-2"
+                />
+                <p className="text-neutral-600">
+                    Your space for real News.
+                </p>
+            </div>
             <Login_Form />
         </main>
     )
