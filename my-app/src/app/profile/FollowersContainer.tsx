291f69a757b0b4534dec5553efa04ac178405557
JaafariMostafa <mostafa.business.me@gmail.com>
2025-07-17 19:05:57 +0000

    .

--- my-app/src/app/profile/FollowersContainer.tsx
+++ my-app/src/app/profile/FollowersContainer.tsx
@@ -1,70 +1,106 @@
 "use client";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import FollowersContainerHeader from './FollowersContainerHeader'
 import { useEditProfile } from '@/context/OpenEditProfileContext';
 import { useUserInfo } from '@/context/UserInfoContext';
-import { collection, onSnapshot, query, where } from 'firebase/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from '@/Firebase';
+import Image from 'next/image';
 
+
+export interface userDataTypes {
+  email: string;
+  followers: string[];
+  following: string[];
+  name: string;
+  profileimage: string;
+  profilebio: string;
+  isPrivateAccount: boolean;
+  Links: {
+    label: string;
+    link: string;
+  }[];
+  interests: string[];
+}
 export default function FollowersContainer() {
   const { currentFTab } = useEditProfile();
-  const { Following, email, Followers } = useUserInfo();
+  const { Following, Followers } = useUserInfo();
 
   const CurrentTabSelected = currentFTab === "following" ? Following : Followers;
+  const [usersData, setUsersData] = useState<userDataTypes[] | []>([]);
+  const [isUsersDataLoading, setIsUsersDataLoading] = useState<boolean>(true)
   useEffect(() => {
     const usersRef = collection(db, "users");
-    const unsubscribe = onSnapshot(usersRef, (snapshot) =)
-    const followedUsers = snapshot.docs
-    .filter(doc => followingArray.includes(doc.id))
-    .map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-  },[currentFTab])
+    const unsubscribe = onSnapshot(usersRef, (snapshot) => {
+      const followedUsers = snapshot.docs
+      .filter(doc => CurrentTabSelected?.includes(doc.id))
+      .map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setUsersData(followedUsers)
+      setIsUsersDataLoading(false);
+    })
+    return () => unsubscribe();
+  },[currentFTab, CurrentTabSelected])
+  if(isUsersDataLoading) return <div>Loading...</div>
   return (
     <section
-        className="fixed left-0 top-0 bg-black/50 
-          z-50 w-full h-screen flex flex-col 
-          items-center justify-start pt-14"
-    >
-        <div
-            className="lg:w-1/2 rounded-3xl bg-[#101010]
-            border border-neutral-800 overflow-hidden"
-            >
-          <FollowersContainerHeader />
+          className="fixed left-0 top-0 bg-black/50 
+            z-50 w-full h-screen flex flex-col 
+            items-center justify-start pt-14"
+        >
             <div
-              className='w-full flex items-center gap-4 pl-6 mb-2'
+                className="lg:w-1/2 rounded-3xl bg-[#101010]
+                  border border-neutral-800 overflow-hidden"
             >
-              <div
-                className='relative flex-shrink-0 w-12 h-12 rounded-full border 
-                  overflow-hidden'
-              >
-                {/* Image */}
-              </div>
-              <div
-                className='w-full py-4 pr-6 flex items-center justify-between
-                  border-b border-neutral-800'
-              >
-                <span>
-                  <h1
-                    className=''
-                  >
-                    Mostafa Jaafari
-                  </h1>
-                  <p
-                    className='text-neutral-500'
-                  >
-                    username
-                  </p>
-                </span>
-                <button
-                  className='border py-1 px-4 rounded-xl text-neutral-700'
-                >
-                  Follow
-                </button>
-              </div>
+              <FollowersContainerHeader
+                FollowersLength={Followers?.length}
+                FollowingLength={Following?.length}
+              />
+                {usersData.length > 0 && usersData.map((user) => {
+                  return (
+                    <div
+                      key={user?.email}
+                      className='w-full flex items-center gap-4 pl-6 mb-2'
+                    >
+                      <div
+                        className='relative flex-shrink-0 w-12 h-12 rounded-full border 
+                          overflow-hidden'
+                      >
+                        <Image 
+                          src={user?.profileimage}
+                          alt='profile image'
+                          fill
+                          className='object-cover'
+                        />
+                      </div>
+                      <div
+                        className='w-full py-4 pr-6 flex items-center justify-between
+                          border-b border-neutral-800'
+                      >
+                        <span>
+                          <h1
+                            className=''
+                          >
+                            {user?.name}
+                          </h1>
+                          <p
+                            className='text-neutral-500'
+                          >
+                            username
+                          </p>
+                        </span>
+                        <button
+                          className='border py-1 px-4 rounded-xl text-neutral-700'
+                        >
+                          {currentFTab === "followers" ? "Follow Back" : "Following"}
+                        </button>
+                      </div>
+                    </div>
+                  )
+                })}
             </div>
-        </div>
     </section>
   )
 }
--- my-app/src/app/profile/FollowersContainerHeader.tsx
+++ my-app/src/app/profile/FollowersContainerHeader.tsx
@@ -2,8 +2,13 @@
 import { useEditProfile } from '@/context/OpenEditProfileContext';
 import React from 'react';
 
+
+interface FollowersContainerHeaderTypes{
+    FollowersLength: number;
+    FollowingLength: number;
+}
 const HeaderTabs = ["followers", "following"];
-export default function FollowersContainerHeader() {
+export default function FollowersContainerHeader({ FollowersLength, FollowingLength }: FollowersContainerHeaderTypes) {
     const { setCurrentFTab, currentFTab } = useEditProfile();
     
     return (
@@ -15,12 +20,19 @@ export default function FollowersContainerHeader() {
                     <button
                         key={index}
                         onClick={() => setCurrentFTab(tab.toLowerCase())}
-                        className={`w-full flex flex-col 
-                            items-center py-4 cursor-pointer
+                        className={`w-full flex flex-col
+                            items-center py-2 cursor-pointer
                             transition-all duration-100
                             ${currentFTab === tab.toLowerCase() ? "font-bold border-b text-white" : "text-neutral-500"}`}
                     >
-                        {tab}
+                        <h2>
+                            {tab}
+                        </h2>
+                        <span
+                            className='font-thin text-xs'
+                        >
+                            {tab === "followers" ? FollowersLength : FollowingLength}
+                        </span>
                     </button>
                 )
             })}
