c28e5d1eaf2db46c97e867938e05c538cb2de227
JaafariMostafa <mostafa.business.me@gmail.com>
2025-07-18 22:00:29 +0000

    finish public profile header

--- my-app/src/app/profile/[profileid]/PublicProfileHeader.tsx
+++ my-app/src/app/profile/[profileid]/PublicProfileHeader.tsx
@@ -1,41 +1,45 @@
 "use client";
-import { UserInfoContextProps } from '@/context/UserInfoContext';
+import { FollowButton } from '@/Components/Functions/FollowButton';
+import { UserInfoContextProps, useUserInfo } from '@/context/UserInfoContext';
 import { db } from '@/Firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { ChartColumnBig, Instagram } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 
 
 export default function PublicProfileHeader({ UserName }: { UserName: string; }) {
+    const Current_User = useSession()?.data?.user?.email;
     const [UserDetails, setUserDetails] = useState<UserInfoContextProps | null>(null);
     const [loading, setLoading] = useState(false);
+    const { Following } = useUserInfo();
     useEffect(() => {
-    if (!UserName) return;
+        if (!UserName) return;
 
-    const fetchUser = async () => {
-      setLoading(true);
-      try {
-        const q = query(collection(db, "users"), where("username", "==", UserName));
-        const snapshot = await getDocs(q);
+        const fetchUser = async () => {
+        setLoading(true);
+        try {
+            const q = query(collection(db, "users"), where("username", "==", UserName));
+            const snapshot = await getDocs(q);
 
-        if (!snapshot.empty) {
-          setUserDetails(snapshot?.docs[0].data() as UserInfoContextProps);
-        } else {
-          setUserDetails(null); // لم يتم العثور على مستخدم
-        }
-      } catch (error) {
-        console.error("❌ Error fetching user by username:", error);
-      }
-      setLoading(false);
-    };
-
-    fetchUser();
-  }, [UserName]);
-  if(loading) return <div>Loading...</div>
+            if (!snapshot.empty) {
+            setUserDetails(snapshot?.docs[0].data() as UserInfoContextProps);
+            } else {
+            setUserDetails(null); // لم يتم العثور على مستخدم
+            }
+        } catch (error) {
+            console.error("❌ Error fetching user by username:", error);
+            }
+            setLoading(false);
+        };
+        
+        fetchUser();
+    }, [UserName]);
+    const IsFollowed = UserDetails && Following?.includes(UserDetails?.email);
     return (
         <section>
-            {JSON.stringify(UserDetails) || "null"}
             <div
                 className="p-6"
             >
@@ -46,24 +50,51 @@ export default function PublicProfileHeader({ UserName }: { UserName: string; })
                         <h1
                             className="font-bold text-2xl"
                         >
-                            Mostafa Jaafari
+                            {loading ? (
+                                <div className='w-45 h-6 rounded-md bg-neutral-900 animate-pulse'/>
+                            ) : UserDetails?.name}
                         </h1>
                         <p
                             className="text-neutral-300"
                         >
-                            {UserName}
+                            {loading ? (
+                                <div className='w-28 h-4 mt-2 rounded-md bg-neutral-900 animate-pulse'/>
+                            )
+                            :
+                            UserDetails?.username}
                         </p>
                     </span>
-                    <div
+                    {loading ? (
+                                <div className='w-18 h-18 rounded-full border border-neutral-800 bg-neutral-900 animate-pulse'/>
+                            )
+                            :
+                            (
+                                <div
                         className="relative w-18 h-18 flex-shrink-0 
                             rounded-full overflow-hidden border"
-                    ></div>
+                    >
+                        <Image 
+                            src={UserDetails?.profileimage as string}
+                            alt={UserDetails?.username as string}
+                            fill
+                            loading='lazy'
+                            className='object-cover'
+                        />
+                    </div>
+                    )}
                 </div>
+                <p
+                    className='text-neutral-500 font-bold'
+                >
+                    {UserDetails?.profilebio}
+                </p>
                 <div
                     className="w-full py-4 flex items-center justify-between"
                 >
-                    <span>
-                        4 followers
+                    <span
+                        className="text-neutral-500 text-sm hover:underline cursor-pointer"
+                    >
+                        {UserDetails?.Followers?.length} followers
                     </span>
                     <div
                         className='flex items-center gap-2'
@@ -90,10 +121,15 @@ export default function PublicProfileHeader({ UserName }: { UserName: string; })
                     className='w-full flex gap-2 items-center justify-between'
                 >
                     <button
-                        className='w-full bg-neutral-200 text-neutral-900 cursor-pointer 
-                            hover:bg-neutral-300 py-1.5 text-sm font-bold rounded-xl'
+                        onClick={async () => {
+                            if(!UserDetails || !Current_User) return;
+                            await FollowButton(UserDetails?.email, Current_User);
+                            }}
+                        className={`w-full cursor-pointer border
+                            py-1.5 text-sm font-bold rounded-xl
+                            ${loading ? "text-neutral-800 bg-neutral-900 border-neutral-800 animate-pulse" : IsFollowed ? "border-neutral-800 hover:border-neutral-900 text-neutral-500" : "border-neutral-800 bg-neutral-900/50 text-neutral-200"}`}
                     >
-                        Follow Back
+                        {loading ? "..." : IsFollowed ? "Following" : "Follow"}
                     </button>
                     <button
                         className='w-full bg-neutral-800 border border-neutral-700 
